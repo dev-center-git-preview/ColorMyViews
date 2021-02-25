fe60938fc85fbb67252ffc87deacf5bcca3f5582
dev-center-git-preview <79448806+dev-center-git-preview@users.noreply.github.com>
2021-02-25 17:20:03 +0200

    Added Listeners to the text views

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        var clickableViews = listOf<View>(
+                findViewById(R.id.box_one_text),
+                findViewById(R.id.box_two_text),
+                findViewById(R.id.box_three_text),
+                findViewById(R.id.box_foure_text),
+                findViewById(R.id.box_five_text))
+
+        for (view in clickableViews) {
+            view.setOnClickListener { setViewColor(it) }
+        }
+    }
+
+    private fun setViewColor(view: View) {
+        when(view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_foure_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            else -> view.setBackgroundColor(Color.GRAY)
+        }
     }
 }
